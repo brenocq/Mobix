7e72459744ea8d8fc4e87eca63fecb45b2d4aa27
Breno Cunha Queiroz <breno1423@gmail.com>
2019-05-06 01:07:08 -0300

    Fix ROSmsgs erros

--- Software/MobixSrc/lib/ros_lib/rosserial_arduino/Move.h
+++ Software/MobixSrc/lib/ros_lib/rosserial_arduino/Move.h
@@ -12,15 +12,12 @@ namespace rosserial_arduino
   class Move : public ros::Msg
   {
     public:
-      uint32_t vel_length;
-      typedef float _vel_type;
-      _vel_type st_vel;
-      _vel_type * vel;
+      float vel[2];
       typedef float _time_type;
       _time_type time;
 
     Move():
-      vel_length(0), vel(NULL),
+      vel(),
       time(0)
     {
     }
@@ -28,12 +25,7 @@ namespace rosserial_arduino
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
-      *(outbuffer + offset + 0) = (this->vel_length >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (this->vel_length >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (this->vel_length >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (this->vel_length >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->vel_length);
-      for( uint32_t i = 0; i < vel_length; i++){
+      for( uint32_t i = 0; i < 2; i++){
       union {
         float real;
         uint32_t base;
@@ -61,27 +53,18 @@ namespace rosserial_arduino
     virtual int deserialize(unsigned char *inbuffer)
     {
       int offset = 0;
-      uint32_t vel_lengthT = ((uint32_t) (*(inbuffer + offset)));
-      vel_lengthT |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      vel_lengthT |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      vel_lengthT |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      offset += sizeof(this->vel_length);
-      if(vel_lengthT > vel_length)
-        this->vel = (float*)realloc(this->vel, vel_lengthT * sizeof(float));
-      vel_length = vel_lengthT;
-      for( uint32_t i = 0; i < vel_length; i++){
+      for( uint32_t i = 0; i < 2; i++){
       union {
         float real;
         uint32_t base;
-      } u_st_vel;
-      u_st_vel.base = 0;
-      u_st_vel.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_st_vel.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_st_vel.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_st_vel.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->st_vel = u_st_vel.real;
-      offset += sizeof(this->st_vel);
-        memcpy( &(this->vel[i]), &(this->st_vel), sizeof(float));
+      } u_veli;
+      u_veli.base = 0;
+      u_veli.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_veli.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_veli.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_veli.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->vel[i] = u_veli.real;
+      offset += sizeof(this->vel[i]);
       }
       union {
         float real;
@@ -98,7 +81,7 @@ namespace rosserial_arduino
     }
 
     const char * getType(){ return "rosserial_arduino/Move"; };
-    const char * getMD5(){ return "0b6068eb99ee71bef896eeefe86c9553"; };
+    const char * getMD5(){ return "636357bd9e8164bd16b6d25b1a3fd768"; };
 
   };
 
--- Software/MobixSrc/lib/ros_lib/rosserial_arduino/Sensors.h
+++ Software/MobixSrc/lib/ros_lib/rosserial_arduino/Sensors.h
@@ -12,35 +12,19 @@ namespace rosserial_arduino
   class Sensors : public ros::Msg
   {
     public:
-      uint32_t laser_length;
-      typedef int32_t _laser_type;
-      _laser_type st_laser;
-      _laser_type * laser;
-      uint32_t IMU_length;
-      typedef float _IMU_type;
-      _IMU_type st_IMU;
-      _IMU_type * IMU;
-      uint32_t IRReceiver_length;
-      typedef int32_t _IRReceiver_type;
-      _IRReceiver_type st_IRReceiver;
-      _IRReceiver_type * IRReceiver;
+      int32_t laser[6];
+      float IMU[9];
 
     Sensors():
-      laser_length(0), laser(NULL),
-      IMU_length(0), IMU(NULL),
-      IRReceiver_length(0), IRReceiver(NULL)
+      laser(),
+      IMU()
     {
     }
 
     virtual int serialize(unsigned char *outbuffer) const
     {
       int offset = 0;
-      *(outbuffer + offset + 0) = (this->laser_length >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (this->laser_length >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (this->laser_length >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (this->laser_length >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->laser_length);
-      for( uint32_t i = 0; i < laser_length; i++){
+      for( uint32_t i = 0; i < 6; i++){
       union {
         int32_t real;
         uint32_t base;
@@ -52,12 +36,7 @@ namespace rosserial_arduino
       *(outbuffer + offset + 3) = (u_laseri.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->laser[i]);
       }
-      *(outbuffer + offset + 0) = (this->IMU_length >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (this->IMU_length >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (this->IMU_length >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (this->IMU_length >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->IMU_length);
-      for( uint32_t i = 0; i < IMU_length; i++){
+      for( uint32_t i = 0; i < 9; i++){
       union {
         float real;
         uint32_t base;
@@ -69,100 +48,43 @@ namespace rosserial_arduino
       *(outbuffer + offset + 3) = (u_IMUi.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->IMU[i]);
       }
-      *(outbuffer + offset + 0) = (this->IRReceiver_length >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (this->IRReceiver_length >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (this->IRReceiver_length >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (this->IRReceiver_length >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->IRReceiver_length);
-      for( uint32_t i = 0; i < IRReceiver_length; i++){
-      union {
-        int32_t real;
-        uint32_t base;
-      } u_IRReceiveri;
-      u_IRReceiveri.real = this->IRReceiver[i];
-      *(outbuffer + offset + 0) = (u_IRReceiveri.base >> (8 * 0)) & 0xFF;
-      *(outbuffer + offset + 1) = (u_IRReceiveri.base >> (8 * 1)) & 0xFF;
-      *(outbuffer + offset + 2) = (u_IRReceiveri.base >> (8 * 2)) & 0xFF;
-      *(outbuffer + offset + 3) = (u_IRReceiveri.base >> (8 * 3)) & 0xFF;
-      offset += sizeof(this->IRReceiver[i]);
-      }
       return offset;
     }
 
     virtual int deserialize(unsigned char *inbuffer)
     {
       int offset = 0;
-      uint32_t laser_lengthT = ((uint32_t) (*(inbuffer + offset))); 
-      laser_lengthT |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1); 
-      laser_lengthT |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2); 
-      laser_lengthT |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3); 
-      offset += sizeof(this->laser_length);
-      if(laser_lengthT > laser_length)
-        this->laser = (int32_t*)realloc(this->laser, laser_lengthT * sizeof(int32_t));
-      laser_length = laser_lengthT;
-      for( uint32_t i = 0; i < laser_length; i++){
+      for( uint32_t i = 0; i < 6; i++){
       union {
         int32_t real;
         uint32_t base;
-      } u_st_laser;
-      u_st_laser.base = 0;
-      u_st_laser.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_st_laser.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_st_laser.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_st_laser.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->st_laser = u_st_laser.real;
-      offset += sizeof(this->st_laser);
-        memcpy( &(this->laser[i]), &(this->st_laser), sizeof(int32_t));
+      } u_laseri;
+      u_laseri.base = 0;
+      u_laseri.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_laseri.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_laseri.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_laseri.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->laser[i] = u_laseri.real;
+      offset += sizeof(this->laser[i]);
       }
-      uint32_t IMU_lengthT = ((uint32_t) (*(inbuffer + offset))); 
-      IMU_lengthT |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1); 
-      IMU_lengthT |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2); 
-      IMU_lengthT |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3); 
-      offset += sizeof(this->IMU_length);
-      if(IMU_lengthT > IMU_length)
-        this->IMU = (float*)realloc(this->IMU, IMU_lengthT * sizeof(float));
-      IMU_length = IMU_lengthT;
-      for( uint32_t i = 0; i < IMU_length; i++){
+      for( uint32_t i = 0; i < 9; i++){
       union {
         float real;
         uint32_t base;
-      } u_st_IMU;
-      u_st_IMU.base = 0;
-      u_st_IMU.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_st_IMU.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_st_IMU.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_st_IMU.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->st_IMU = u_st_IMU.real;
-      offset += sizeof(this->st_IMU);
-        memcpy( &(this->IMU[i]), &(this->st_IMU), sizeof(float));
-      }
-      uint32_t IRReceiver_lengthT = ((uint32_t) (*(inbuffer + offset))); 
-      IRReceiver_lengthT |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1); 
-      IRReceiver_lengthT |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2); 
-      IRReceiver_lengthT |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3); 
-      offset += sizeof(this->IRReceiver_length);
-      if(IRReceiver_lengthT > IRReceiver_length)
-        this->IRReceiver = (int32_t*)realloc(this->IRReceiver, IRReceiver_lengthT * sizeof(int32_t));
-      IRReceiver_length = IRReceiver_lengthT;
-      for( uint32_t i = 0; i < IRReceiver_length; i++){
-      union {
-        int32_t real;
-        uint32_t base;
-      } u_st_IRReceiver;
-      u_st_IRReceiver.base = 0;
-      u_st_IRReceiver.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
-      u_st_IRReceiver.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
-      u_st_IRReceiver.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
-      u_st_IRReceiver.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
-      this->st_IRReceiver = u_st_IRReceiver.real;
-      offset += sizeof(this->st_IRReceiver);
-        memcpy( &(this->IRReceiver[i]), &(this->st_IRReceiver), sizeof(int32_t));
+      } u_IMUi;
+      u_IMUi.base = 0;
+      u_IMUi.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_IMUi.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_IMUi.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_IMUi.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->IMU[i] = u_IMUi.real;
+      offset += sizeof(this->IMU[i]);
       }
      return offset;
     }
 
     const char * getType(){ return "rosserial_arduino/Sensors"; };
-    const char * getMD5(){ return "4a783ba88292007d232f848dc33ea4e9"; };
+    const char * getMD5(){ return "56c991a6af25c6567fa4cc133378b992"; };
 
   };
 
